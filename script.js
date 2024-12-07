d249090829c09be1feaa128055731c2c29642580
Nivedita <niveditachhokar@gmail.com>
2024-12-07 22:55:01 +0530

    Added CSS and linked html file with js

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 const fileInput = document.querySelector("input");
 const downloadBtn = document.querySelector("button");
 
-downloadBtn.addEventListener("click",e =>{
+downloadBtn.addEventListener("click", (e) =>{
     e.preventDefault();
     downloadBtn.innerText="Downloading File...";
     fetchFile(fileInput.value);
 });
 
 function fetchFile(url){
-    fetch(url).then(res.blob()).then(file => {
-        let tempUrl = URL.createObjectURL(file);
-        let aTag = document.createElement("a");
-        aTag.href= tempUrl;
-        aTag.download = "filename";
-        // console.log(file);
-        document.body.appendChild(aTag);
-        aTag.click();
-        aTag.remove();
-        URL.revokeObjectURL(tempUrl);
-        downloadBtn.innerText = "Download File";
-    }).catch(()=>{
-        alert("Failed to download file");
-    })
+    console.log("Fetching file from: ", url);
+    fetch(url)
+        .then((res) => {
+            if (!res.ok) throw new Error("Network response was not ok");
+            return res.blob(); 
+        })
+        .then((file) => {
+            let tempUrl = URL.createObjectURL(file);
+            let aTag = document.createElement("a");
+            aTag.href = tempUrl;
+            aTag.download = url.split('/').pop();
+            document.body.appendChild(aTag);
+            aTag.click();
+            aTag.remove();
+            URL.revokeObjectURL(tempUrl);
+            downloadBtn.innerText = "Download File";
+    }).catch((err)=>{
+        console.log("ERROR: ", err);
+        alert("Failed to download file. Please check the URL");
+        downloadBtn.innerText= "Download File";
+    });
 }
